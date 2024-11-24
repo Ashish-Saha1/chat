38cd991e05bdcaf86d6ae25a2721d7768fa194aa
ashish-saha <rubelsaha1@gmail.com>
2024-11-25 00:01:19 +0600

    2nd commit not found error.js work form home

--- Middleware/errorHandler.js
+++ Middleware/errorHandler.js
@@ -1,6 +1,17 @@
 const httpErrors = require('http-errors');
 
 
-function notFoundHandler(){
-    next("")
+function notFoundHandler(req,res,next){
+    next(createError(401, 'Requested page not found'))
+}
+
+
+function defaultHandler(err,req,res,next){
+    res.render('error')
+}
+
+
+module.exports = {
+    notFoundHandler,
+    defaultHandler
 }
--- app.js
+++ app.js
@@ -1,9 +1,26 @@
-const cookieParser = require('cookie-parser');
+
 const express = require('express');
 const dotEnv = require('dotenv').config();
 const cookieParser = require('cookie-parser')
+const path = require('path');
+const mongoose = require('mongoose');
+const ejs = require('ejs');
+const {notFoundHandler, defaultHandler } = require('./Middleware/errorHandler');
 
 const app = express();
+
+//Mongodb Connection
+mongoose.connect(process.env.MONGDB_STRING)
+.then(()=>console.log('Mongodb Connected'))
+.catch((err)=>console.log(err))
+
+
+
+// View engine set
+app.set('view engine', 'ejs')
+
+//Static folder
+app.use(express.static(path.join(__dirname, "./Public/")))
 
 //parse Data
 app.use(express.json());
@@ -15,3 +32,10 @@ app.use(cookieParser(process.env.COOKIE_SECRET))
 
 //Error Handle Not found
 app.use(notFoundHandler)
+
+
+//Error Handle Default
+app.use(defaultHandler)
+
+
+app.listen(process.env.PORT, ()=>console.log(`Listing on port ${process.env.PORT}`))

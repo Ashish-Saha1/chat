77874550637c5ac72b3685794eb07cc34ef38d1d
Ashish Saha <rubelsaha1@gamil.com>
2024-12-09 17:54:59 +0600

    from office working done validation tutorial 4

--- Middleware/Common/decorateHtmlResponse.js
+++ Middleware/Common/decorateHtmlResponse.js
@@ -3,6 +3,9 @@ function decorateHtmlResponse(title_name){
     return (req,res,next)=>{
         res.locals.html = true;
         res.locals.title = `${title_name} - ${process.env.APP_NAME}`;
+        res.locals.loggedInUser = {};
+        res.locals.errors = {};
+        res.locals.data = {};
         next()
     }
 }
--- Router/loginRouter.js
+++ Router/loginRouter.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const { getLogin, login } = require('../Controller/loginController')
-const decorateHtmlResponse = require('../Middleware/Common/decorateHtmlResponse')
+const decorateHtmlResponse = require('../Middleware/Common/decorateHtmlResponse');
+const { doLoginValidator, doLoginValidationHandler } = require("../Middleware/login/loginValidator")
 
 router.get("/", decorateHtmlResponse("Login"), getLogin)
 
-router.post('/', login)
+router.post('/',decorateHtmlResponse('Login'),doLoginValidator, doLoginValidationHandler, login)
 
 
 
